3d8b48846e4139b9a7c42f0e812a4bad864313bc
Azure <jc20abalajon@gmail.com>
2026-06-22 18:55:22 +0800

    initial commit for job application tracker

---
+++ backend/src/index.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+const pool = new Pool({
+
+    connectionString : process.env.DATABASE_URL,
+
+});
+
+app.get('/', (req, res) => {
+    res.send('Job Tracker API is running');
+});
+
+
+app.get('/applications', async (req, res) => {
+
+    const result = await pool.query('SELECT * FROM applications');
+    res.json(result.rows);
+
+});
+
+app.post('/applications', async (req, res) => {
+
+    const {company_name, role_title} = req.body;
+
+    const result = await pool.query(
+
+        'INSERT INTO applications(company_name, role_title) VALUES ($1, $2) RETURNING *',
+        [company_name, role_title]
+
+    );
+
+    res.status(201).json(result.rows[0])
+
+});
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server running on http://localhost:${PORT}`);
+    
+});
